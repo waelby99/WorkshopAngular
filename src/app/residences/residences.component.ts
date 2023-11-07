a1c6103974e2db354b485f6b120566e211b1d147
waelby99 <waelbenyoussef19991@gmail.com>
2023-11-07 11:43:30 +0100

    firstcommit

--- src/app/residences/residences.component.ts
+++ src/app/residences/residences.component.ts
@@ -8,13 +8,13 @@ import { Residence } from '../core/models/Residence';
 export class ResidencesComponent {
   listResidences:Residence[]=[
     {id:1,"name": "El fel","address":"Borj Cedria",
-    "image":"../../assets/images/R1.jpeg"},
+    "image":"../../assets/images/taswirav1.jfif"},
     {id:2,"name": "El yasmine",
-    "address":"Ezzahra","image":"../../assets/images/R2.jpg"},
+    "address":"Ezzahra","image":"../../assets/images/taswirav2.jfif"},
     {id:3,"name": "El Arij",
-    "address":"Rades","image":"../../assets/images/R3.jpg"},
+    "address":"Rades","image":"../../assets/images/taswirav3.jfif"},
     {id:4,"name": "El Anber","address":"Manzah 5",
-    "image":"../../assets/images/R4.jpg"}
+    "image":"../../assets/images/taswirav4.jfif"}
     ];
 
 }
